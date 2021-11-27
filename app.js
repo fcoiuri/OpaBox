b20e93d23046e22e3ba1a1e7cd00fad1c521d0c8
fcoiuri <fcoiuri@gmail.com>
2021-11-26 22:28:40 -0300

    finalizando o projeto

--- app.js
+++ app.js
@@ -1,101 +1,101 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes");
-const flash = require("express-flash");
-const path = require("path");
-const jsdom = require('jsdom');
-const $ = require( "jquery" )(new jsdom.JSDOM().window);
+const axios = require("axios");
+const url = "http://localhost:8080/interlace";
+
+const db = require("./db.json");
 
 app.set("view engine", "ejs");
-app.set("views", "./views");
-app.use(express.static(__dirname + '/views/'));
-// app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
-app.use(express.static(__dirname + '/src/'));
+app.set("views", "./src/views");
 
-//app.use(express.static(__dirname + '/resources'));
+app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/src/views'));
+
 app.listen(process.env.PORT || 8080);
 
-app.get("/", function (req, res) {
-    var ab = "<h2>Ok1</h2>";
-    $("#testando").html(ab);
-    res.render("index.ejs", {teste1: ab});
+
+app.get("/interlace", (req, res) => {
+    res.json({ db });
 })
 
+const getIntervalos =  async () => {
+    try {
+        return await axios.get(url)
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+const getIntervalosAsync = async () =>{
+    const inter = await getIntervalos()
+    if(inter.data.db){
+        const intervalos = inter.data;
+        //recebe o valor do intervaloA
+        var A = intervalos.db.intervaloA;
+        //recebe o valor do intervaloA
+        var B = intervalos.db.intervaloB;
+        
+        //recebe a divisão de A
+        const divA = A.reduce((partial_sum, a) =>
+        a / partial_sum
+        );
+
+        //recebe a divisão de B
+        const divB = B.reduce((partial_sum, a) =>
+        a / partial_sum
+
+        );
+
+         /*subtrai A[1] por A[0] para verificar se 
+            A[1] é igual a B[0]
+            */
+        const seqA = A.reduce((partial_sum, a) =>
+                a - partial_sum
+        );
+
+        //verifica se  interlaçam ou sobrepõe
+        var verificandoInterla = divA === divB ? 
+        true : B[0] === A[1] && B[1] === B[0] + seqA ? 
+        true : false;
+
+       app.get("/", function (req, res) {
+        getIntervalosAsync()
+    
+        res.render("index.ejs", {intervaloA: A, 
+            intervaloB: B,getInterval: verificandoInterla});
+    })
+}
+}
+
+//chamando o get '/'
+getIntervalosAsync();
+
 app.use(express.json());
-app.use(express.urlencoded());
-// app.use(express.urlencoded({
-//     extended: true
-// }))
-
-// function teste(){
-//     return "função ok"
-// }
-
-// app.locals.teste = teste;
-
+app.use(express.urlencoded({extended: true}));
 
 app.post('/ordenaLista', (req, res) => {
-    // const salaN = parseInt(req.body.listas.salaN);
-    console.log(req.body);
-    //
     
-    const texto = JSON.stringify(req.body.listas);
     const salaS = JSON.stringify(req.body.listas.salaS);
-    console.log(salaS);
     var resp = [];
     const salaS1 = JSON.parse(salaS);
 
-    for(var i in salaS1){
+    for (var i in salaS1) {
         resp.push(salaS1[i]);
     }
-    var apresentar = resp.sort();
-
-    //
+    var apresentarS = resp.sort();
+    
     const salaN = JSON.stringify(req.body.listas.salaN);
-    console.log(salaN);
     var respN = [];
     const salaN1 = JSON.parse(salaN);
-    for(let i in salaN1){
+    for (let i in salaN1) {
         respN.push(salaN1[i]);
     }
 
-    var apresentarN = respN.sort();
-    console.log(apresentarN);
+    var arraydeNumeros = respN.map(Number);
+    const compararNumeros = (a, b) => a - b;
+    var apresentarN = arraydeNumeros.sort(compararNumeros);
 
-    /*
-    console.log(salaN);
-    // element.getElementById("testa")
-    // .innerHTML =  req.body.listas;
-    //...
-    //salaS: [ “a”, “x”, “n” ]
-    //retorna salaS: [ “a”, “n”, “x” ]
-    
-    const salaS = JSON.stringify(req.body.listas.salaS);
-    // const salaS1 = salaS.split();
-    const salaS1 = JSON.parse(salaS);
-    var resp = [];
-    // const salaS2 = salaS1.sort();
-    for(var i in salaS1){
-        resp.push(salaS1[i]);
-    }
-    var apresentar = resp.sort();
-
-    // salaS.sort((a,b) => {
-    //     return a
-    // })
-
-    console.log(`tipo: ${typeof salaS1}`);
-    console.log(req.body.listas.salaS);
-
-    function teste1(){
-        return apresentar;
-    }
-
-    app.locals.teste1 = teste1;
-    var a = "<h2>Ok</h2>";
-    $("#testa").html(a);
-    */
-    // res.render("teste.ejs");
-    res.render("ordenaLista.ejs", {dados: apresentar, dadosN: apresentarN});
+    res.render("ordenaLista.ejs", { dadosS: apresentarS, dadosN: apresentarN });
     res.end();
 })
--- src/addCampos.js
+++ src/addCampos.js
@@ -1,18 +1,40 @@
-function add_field(){
+document.getElementById ("botaoSalaN").addEventListener ("click", add_campoN, false);
+function add_campoN(){
+  
+    var formu = document.getElementById("form");
+    // criando um input pra inserir
+    var novo_campo = document.createElement("input");
+    // colocando o tipo do input
+    novo_campo.setAttribute("type", "number");
+    // colocando o nome do input e especificacoes
+    novo_campo.setAttribute("name", "listas[salaN][]");
+    novo_campo.setAttribute("class", "form-control ");
+    novo_campo.setAttribute("placeholder", "Sala N");
+    novo_campo.setAttribute("required", "required");
+    // insere o elemento na penúltima posição
+    var pos = formu.childElementCount;
 
-    var x = document.getElementById("form");
-    // create an input field to insert
-    var new_field = document.createElement("input");
-    // set input field data type to text
-    new_field.setAttribute("type", "number");
-    // set input field name 
-    new_field.setAttribute("name", "listas[salaN][]");
-    new_field.setAttribute("class", "form-control ");
-    new_field.setAttribute("placeholder", "Sala N");
-    new_field.setAttribute("required", "required");
-    // select last position to insert element before it
-    var pos = x.childElementCount;
+    // insere o elemento
+    formu.insertBefore(novo_campo, formu.childNodes[pos]);
+}
 
-    // insert element
-    x.insertBefore(new_field, x.childNodes[pos]);
+document.getElementById ("botaoSalaS").addEventListener ("click", add_campoNS, false);
+
+function add_campoNS() {
+
+    var formu = document.getElementById("form1");
+    // criando um input pra inserir
+    var novo_campo = document.createElement("input");
+    // colocando o tipo do input
+    novo_campo.setAttribute("type", "text");
+    // colocando o nome do input e especificacoes
+    novo_campo.setAttribute("name", "listas[salaS][]");
+    novo_campo.setAttribute("class", "form-control ");
+    novo_campo.setAttribute("placeholder", "Sala S");
+    novo_campo.setAttribute("required", "required");
+    // insere o elemento na penúltima posiçã
+    var pos = formu.childElementCount;
+
+    // insere o elemento
+    formu.insertBefore(novo_campo, formu.childNodes[pos]);
 }
